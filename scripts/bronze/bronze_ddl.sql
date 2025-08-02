fe021b4604479c1a9b7a5b31299e4b05537d91b1
Shantanu Pandhre <115913069+shantanu-04@users.noreply.github.com>
2025-08-02 11:26:09 +0530

    Update bronze_ddl.sql

--- scripts/bronze/bronze_ddl.sql
+++ scripts/bronze/bronze_ddl.sql
@@ -1,17 +1,15 @@
 /*
-
-CREATE DDL SCRIPT: BRONZE LAYER
+CREATE DDL(Data Definition Language) SCRIPT: BRONZE LAYER
 
 script purpose: To create tables in the bronze schema, and drop existing tables if they already exist.
 
 warning: This script re-defines the DDL structure of the 'bronze' tables
-
 */
 
 -- table: crm_cust_info
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL -- dropping the table if it already exists
 	DROP TABLE bronze.crm_cust_info;
-CREATE TABLE bronze.crm_cust_info (
+CREATE TABLE bronze.crm_cust_info ( -- defining the columns and data types
 	cst_id INT,
 	cst_key NVARCHAR(50),
 	cst_firstname NVARCHAR(50),
@@ -23,9 +21,9 @@ CREATE TABLE bronze.crm_cust_info (
 GO
 
 -- table: crm_prd_info
-IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL -- dropping the table if it already exists
 	DROP TABLE bronze.crm_prd_info;
-CREATE TABLE bronze.crm_prd_info (
+CREATE TABLE bronze.crm_prd_info ( -- defining the columns and data types
 	prd_id INT,
 	prd_key NVARCHAR(50),
 	prd_nm NVARCHAR(50),
@@ -37,9 +35,9 @@ CREATE TABLE bronze.crm_prd_info (
 GO
 
 -- table: crm_sales_details
-IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL -- dropping the table if it already exists
 	DROP TABLE bronze.crm_sales_details;
-CREATE TABLE bronze.crm_sales_details (
+CREATE TABLE bronze.crm_sales_details ( -- defining the columns and data types
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
@@ -53,9 +51,9 @@ CREATE TABLE bronze.crm_sales_details (
 GO
 
 -- table: erp_cust_az12
-IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL -- dropping the table if it already exists
 	DROP TABLE bronze.erp_cust_az12;
-CREATE TABLE bronze.erp_cust_az12 (
+CREATE TABLE bronze.erp_cust_az12 ( -- defining the columns and data types
 	cid NVARCHAR(50),
 	bdate DATE,
 	gen NVARCHAR(50)
@@ -63,18 +61,18 @@ CREATE TABLE bronze.erp_cust_az12 (
 GO
 
 -- table: erp_loc_a101
-IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL -- dropping the table if it already exists
 	DROP TABLE bronze.erp_loc_a101;
-CREATE TABLE bronze.erp_loc_a101 (
+CREATE TABLE bronze.erp_loc_a101 ( -- defining the columns and data types
 	cid NVARCHAR(50),
 	cntry NVARCHAR(50)
 );
 GO
 
 -- table: erp_px_cat_g1v2
-IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL -- dropping the table if it already exists
 	DROP TABLE bronze.erp_px_cat_g1v2;
-CREATE TABLE bronze.erp_px_cat_g1v2 (
+CREATE TABLE bronze.erp_px_cat_g1v2 ( -- defining the columns and data types
 	id VARCHAR(50),
 	cat VARCHAR(50),
 	subcat VARCHAR(50),
