ed75d687ab3708172fa2a27bb16ad138acf51acc
Shantanu Pandhre <115913069+shantanu-04@users.noreply.github.com>
2025-08-02 11:34:28 +0530

    Update silver_ddl.sql

--- scripts/silver/silver_ddl.sql
+++ scripts/silver/silver_ddl.sql
@@ -1,15 +1,14 @@
 /*
-DDL Script: Create Silver Tables
+CREATE DDL SCRIPT: SILVER LAYER
 
 Script Purpose:
     This script creates tables in the 'silver' schema, dropping existing table if they already exist. Run this script to re-define the DDL structure of 'silver' tables.
 */
 
-IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+-- table: silver.crm_cust_info
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL -- dropping the table if it already exists
     DROP TABLE silver.crm_cust_info;
-GO
-
-CREATE TABLE silver.crm_cust_info (
+CREATE TABLE silver.crm_cust_info ( -- defining columns and data types
     cst_id             INT,
     cst_key            NVARCHAR(50),
     cst_firstname      NVARCHAR(50),
@@ -17,15 +16,14 @@ CREATE TABLE silver.crm_cust_info (
     cst_marital_status NVARCHAR(50),
     cst_gndr           NVARCHAR(50),
     cst_create_date    DATE,
-    dwh_create_date    DATETIME2 DEFAULT GETDATE()
+    dwh_create_date    DATETIME2 DEFAULT GETDATE() -- metadata for the table being created
 );
 GO
 
-IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+-- table: silver.crm_prd_info
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL -- dropping the table if it already exists
     DROP TABLE silver.crm_prd_info;
-GO
-
-CREATE TABLE silver.crm_prd_info (
+CREATE TABLE silver.crm_prd_info ( -- defining columns and data types
     prd_id          INT,
     cat_id          NVARCHAR(50),
     prd_key         NVARCHAR(50),
@@ -34,15 +32,14 @@ CREATE TABLE silver.crm_prd_info (
     prd_line        NVARCHAR(50),
     prd_start_dt    DATE,
     prd_end_dt      DATE,
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+    dwh_create_date DATETIME2 DEFAULT GETDATE() -- metadata for the table being created
 );
 GO
 
-IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+-- table: silver.crm_sales_details
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL -- dropping the table if it already exists
     DROP TABLE silver.crm_sales_details;
-GO
-
-CREATE TABLE silver.crm_sales_details (
+CREATE TABLE silver.crm_sales_details ( -- defining columns and data types
     sls_ord_num     NVARCHAR(50),
     sls_prd_key     NVARCHAR(50),
     sls_cust_id     INT,
@@ -52,42 +49,39 @@ CREATE TABLE silver.crm_sales_details (
     sls_sales       INT,
     sls_quantity    INT,
     sls_price       INT,
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+    dwh_create_date DATETIME2 DEFAULT GETDATE() -- metadata for the table being created
 );
 GO
 
-IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+-- table: silver.erp_loc_a101
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL -- dropping the table if it already exists
     DROP TABLE silver.erp_loc_a101;
-GO
-
-CREATE TABLE silver.erp_loc_a101 (
+CREATE TABLE silver.erp_loc_a101 ( -- defining columns and data types
     cid             NVARCHAR(50),
     cntry           NVARCHAR(50),
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+    dwh_create_date DATETIME2 DEFAULT GETDATE() -- metadata for the table being created
 );
 GO
 
-IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+-- table: silver.erp_cust_az12
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL -- dropping the table if it already exists
     DROP TABLE silver.erp_cust_az12;
-GO
-
-CREATE TABLE silver.erp_cust_az12 (
+CREATE TABLE silver.erp_cust_az12 ( -- defining columns and data types
     cid             NVARCHAR(50),
     bdate           DATE,
     gen             NVARCHAR(50),
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+    dwh_create_date DATETIME2 DEFAULT GETDATE() -- metadata for the table being created
 );
 GO
 
-IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+-- table: silver.erp_px_cat_g1v2
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL -- dropping the table if it already exists
     DROP TABLE silver.erp_px_cat_g1v2;
-GO
-
-CREATE TABLE silver.erp_px_cat_g1v2 (
+CREATE TABLE silver.erp_px_cat_g1v2 ( -- defining columns and data types
     id              NVARCHAR(50),
     cat             NVARCHAR(50),
     subcat          NVARCHAR(50),
     maintenance     NVARCHAR(50),
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
+    dwh_create_date DATETIME2 DEFAULT GETDATE() -- metadata for the table being created
 );
 GO
