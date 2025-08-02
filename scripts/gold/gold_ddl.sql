7bdbd4fb44eb16324a7f132ab8311f1ff18f5725
Shantanu Pandhre <115913069+shantanu-04@users.noreply.github.com>
2025-08-02 11:49:05 +0530

    Update gold_ddl.sql

--- scripts/gold/gold_ddl.sql
+++ scripts/gold/gold_ddl.sql
@@ -1,19 +1,19 @@
 /*
-DDL Script: Create Gold Views
-Script Purpose: This script creates views for the Gold layer in the data warehouse. The Gold layer represents the final dimension and fact tables (Star Schema).
-Each view performs transformations and combines data from the Silver layer to produce a clean, enriched, and business-ready dataset.
+CREATE DDL SCRIPT: GOLD LAYER
+Script Purpose: This script creates views for the gold layer in the data warehouse. The gold layer represents the final dimension and fact tables (Star Schema).
+Each view performs transformations and combines data from the silver layer.
 
 Usage: These views can be queried directly for analytics and reporting.
 */
 
--- Create Dimension: gold.dim_customers
-IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+-- create dimension: gold.dim_customers
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL -- dropping and re-creating
     DROP VIEW gold.dim_customers;
 GO
 
 CREATE VIEW gold.dim_customers AS
 SELECT
-    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- defining a surrogate key
     ci.cst_id                          AS customer_id,
     ci.cst_key                         AS customer_number,
     ci.cst_firstname                   AS first_name,
@@ -21,26 +21,26 @@ SELECT
     la.cntry                           AS country,
     ci.cst_marital_status              AS marital_status,
     CASE 
-        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the primary source for gender
-        ELSE COALESCE(ca.gen, 'n/a')  			   -- Fallback to ERP data
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- using the CRM data as it is the primary source for gender
+        ELSE COALESCE(ca.gen, 'n/a')  			   -- ysing ERP data in case CRM data is missing
     END                                AS gender,
     ca.bdate                           AS birthdate,
     ci.cst_create_date                 AS create_date
-FROM silver.crm_cust_info ci
+FROM silver.crm_cust_info ci -- performing left joins
 LEFT JOIN silver.erp_cust_az12 ca
     ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
     ON ci.cst_key = la.cid;
 GO
 
--- Create Dimension: gold.dim_products
-IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+-- create dimension: gold.dim_products
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL -- dropping and re-creating
     DROP VIEW gold.dim_products;
 GO
 
 CREATE VIEW gold.dim_products AS
 SELECT
-    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Surrogate key
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- defining the surrogate key
     pn.prd_id       AS product_id,
     pn.prd_key      AS product_number,
     pn.prd_nm       AS product_name,
@@ -54,11 +54,11 @@ SELECT
 FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
     ON pn.cat_id = pc.id
-WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
+WHERE pn.prd_end_dt IS NULL; -- filtering out all the historical data as it is not needed
 GO
 
--- Create Fact Table: gold.fact_sales
-IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+-- create fact table: gold.fact_sales
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL -- dropping and re-creating
     DROP VIEW gold.fact_sales;
 GO
 
@@ -73,7 +73,7 @@ SELECT
     sd.sls_sales    AS sales_amount,
     sd.sls_quantity AS quantity,
     sd.sls_price    AS price
-FROM silver.crm_sales_details sd
+FROM silver.crm_sales_details sd -- performing left joins
 LEFT JOIN gold.dim_products pr
     ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
