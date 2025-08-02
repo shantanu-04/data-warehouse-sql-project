ac5a8026ee6527f59c4f49171e7dbc1df4e53272
Shantanu Pandhre <115913069+shantanu-04@users.noreply.github.com>
2025-08-02 11:43:56 +0530

    Update silver_proc_load.sql

--- scripts/silver/silver_proc_load.sql
+++ scripts/silver/silver_proc_load.sql
@@ -1,5 +1,5 @@
 /*
-Stored Procedure: Load Silver Layer (Bronze -> Silver)
+STORED PROCEDURE: LOAD SILVER LAYER
 
 Script Purpose: This stored procedure performs the ETL (Extract, Transform, Load) process to fill the 'silver' schema tables from the 'bronze' schema tables.
 There are two main actions:-
@@ -9,9 +9,9 @@ There are two main actions:-
 Usage Example: EXEC Silver.load_silver;
 */
 
-CREATE OR ALTER PROCEDURE silver.load_silver AS
+CREATE OR ALTER PROCEDURE silver.load_silver AS -- to use as stored procedure
 BEGIN
-    DECLARE @batch_start_time DATETIME, @batch_end_time DATETIME; 
+    DECLARE @batch_start_time DATETIME, @batch_end_time DATETIME; -- to calculate load time
     BEGIN TRY
         SET @batch_start_time = GETDATE();
         PRINT 'Loading Silver Layer';
@@ -22,7 +22,7 @@ BEGIN
 		PRINT '------------------------------------------------';
 
 		-- Loading silver.crm_cust_info
-		PRINT '>> Truncating Table: silver.crm_cust_info';
+		PRINT '>> Truncating Table: silver.crm_cust_info'; -- performing truncate and insert function
 		TRUNCATE TABLE silver.crm_cust_info;
 		PRINT '>> Inserting Data Into: silver.crm_cust_info';
 		INSERT INTO silver.crm_cust_info (
@@ -37,18 +37,18 @@ BEGIN
 		SELECT
 			cst_id,
 			cst_key,
-			TRIM(cst_firstname) AS cst_firstname,
-			TRIM(cst_lastname) AS cst_lastname,
+			TRIM(cst_firstname) AS cst_firstname, -- removing whitespaces
+			TRIM(cst_lastname) AS cst_lastname, -- removing whitespaces
 			CASE 
 				WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
 				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
 				ELSE 'n/a'
-			END AS cst_marital_status, -- Normalize marital status values to readable format
+			END AS cst_marital_status, -- normalizing marital status values to a readable and descriptive format
 			CASE 
 				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
 				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
 				ELSE 'n/a'
-			END AS cst_gndr, -- Normalize gender values to readable format
+			END AS cst_gndr, -- normalizing gender values to a readable and descriptive format
 			cst_create_date
 		FROM (
 			SELECT
@@ -57,10 +57,10 @@ BEGIN
 			FROM bronze.crm_cust_info
 			WHERE cst_id IS NOT NULL
 		) t
-		WHERE flag_last = 1; -- Select the most recent record per customer
+		WHERE flag_last = 1; -- selectiing the most recent record per customer from the duplicates
 
 		-- Loading silver.crm_prd_info
-		PRINT '>> Truncating Table: silver.crm_prd_info';
+		PRINT '>> Truncating Table: silver.crm_prd_info'; -- performing truncate and insert function
 		TRUNCATE TABLE silver.crm_prd_info;
 		PRINT '>> Inserting Data Into: silver.crm_prd_info';
 		INSERT INTO silver.crm_prd_info (
@@ -75,26 +75,26 @@ BEGIN
 		)
 		SELECT
 			prd_id,
-			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
-			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
+			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- extracting the category id from the product key
+			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- extracting the product key separated from the product id
 			prd_nm,
-			ISNULL(prd_cost, 0) AS prd_cost,
+			ISNULL(prd_cost, 0) AS prd_cost, -- converting null cost values to 0
 			CASE 
 				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
 				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
 				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
 				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
 				ELSE 'n/a'
-			END AS prd_line, -- Map product line codes to descriptive values
+			END AS prd_line, -- mapping product line codes to descriptive and readable values
 			CAST(prd_start_dt AS DATE) AS prd_start_dt,
 			CAST(
 				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
 				AS DATE
-			) AS prd_end_dt -- Calculate end date as one day before the next start date
+			) AS prd_end_dt -- calculating the end date as one day before the next (leading) start date
 		FROM bronze.crm_prd_info;
 
         	-- Loading crm_sales_details
-		PRINT '>> Truncating Table: silver.crm_sales_details';
+		PRINT '>> Truncating Table: silver.crm_sales_details'; -- performing truncate and insert function
 		TRUNCATE TABLE silver.crm_sales_details;
 		PRINT '>> Inserting Data Into: silver.crm_sales_details';
 		INSERT INTO silver.crm_sales_details (
@@ -115,30 +115,30 @@ BEGIN
 			CASE 
 				WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
 				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
-			END AS sls_order_dt,
+			END AS sls_order_dt, -- checking the format of dates and converting the data type from INT -> VARCHAR -> DATE
 			CASE 
 				WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
 				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
-			END AS sls_ship_dt,
+			END AS sls_ship_dt, -- checking the format of dates and converting the data type from INT -> VARCHAR -> DATE
 			CASE 
 				WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
 				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
-			END AS sls_due_dt,
+			END AS sls_due_dt, -- checking the format of dates and converting the data type from INT -> VARCHAR -> DATE
 			CASE 
 				WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
 					THEN sls_quantity * ABS(sls_price)
 				ELSE sls_sales
-			END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+			END AS sls_sales, -- recalculating sales if original value is missing or incorrect
 			sls_quantity,
 			CASE 
 				WHEN sls_price IS NULL OR sls_price <= 0 
 					THEN sls_sales / NULLIF(sls_quantity, 0)
-				ELSE sls_price  -- Derive price if original value is invalid
+				ELSE sls_price  -- recalculating price if original value is incorrect
 			END AS sls_price
 		FROM bronze.crm_sales_details;
 
         	-- Loading erp_cust_az12
-		PRINT '>> Truncating Table: silver.erp_cust_az12';
+		PRINT '>> Truncating Table: silver.erp_cust_az12'; -- performing truncate and insert function
 		TRUNCATE TABLE silver.erp_cust_az12;
 		PRINT '>> Inserting Data Into: silver.erp_cust_az12';
 		INSERT INTO silver.erp_cust_az12 (
@@ -148,18 +148,18 @@ BEGIN
 		)
 		SELECT
 			CASE
-				WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) -- Remove 'NAS' prefix if present
+				WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) -- removing 'NAS' prefix if present in the record
 				ELSE cid
 			END AS cid, 
 			CASE
 				WHEN bdate > GETDATE() THEN NULL
 				ELSE bdate
-			END AS bdate, -- Set future birthdates to NULL
+			END AS bdate, -- setting future birthdates to NULL as they are clearly invalid
 			CASE
 				WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
 				WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
 				ELSE 'n/a'
-			END AS gen -- Normalize gender values and handle unknown cases
+			END AS gen -- normalizing gender values to a readable and descriptive format and handling unknown cases
 		FROM bronze.erp_cust_az12;
 
 		PRINT '------------------------------------------------';
@@ -167,7 +167,7 @@ BEGIN
 		PRINT '------------------------------------------------';
 
         	-- Loading erp_loc_a101
-		PRINT '>> Truncating Table: silver.erp_loc_a101';
+		PRINT '>> Truncating Table: silver.erp_loc_a101'; -- performing truncate and insert function
 		TRUNCATE TABLE silver.erp_loc_a101;
 		PRINT '>> Inserting Data Into: silver.erp_loc_a101';
 		INSERT INTO silver.erp_loc_a101 (
@@ -181,11 +181,11 @@ BEGIN
 				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
 				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
 				ELSE TRIM(cntry)
-			END AS cntry -- Normalize and Handle missing or blank country codes
+			END AS cntry -- normalizing and handling missing or blank country codes
 		FROM bronze.erp_loc_a101;
 		
 		-- Loading erp_px_cat_g1v2
-		PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
+		PRINT '>> Truncating Table: silver.erp_px_cat_g1v2'; -- performing truncate and insert function
 		TRUNCATE TABLE silver.erp_px_cat_g1v2;
 		PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
 		INSERT INTO silver.erp_px_cat_g1v2 (
@@ -208,7 +208,7 @@ BEGIN
 		PRINT '=========================================='
 		
 	END TRY
-	BEGIN CATCH
+	BEGIN CATCH -- displaying the error in case there occurs one, during the loading process of the silver layer
 		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
 		PRINT 'Error Message' + ERROR_MESSAGE();
 	END CATCH
